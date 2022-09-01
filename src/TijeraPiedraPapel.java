f6ba4674cdfe2c29507f347925c5ea5c50882ec2
edwinmgallego <ing.edwin.gallego@gmail.com>
2022-08-31 20:45:54 -0500

    piedra papel  tijera

---
+++ src/TijeraPiedraPapel.java
@@ -0,0 +1,8 @@
+public class TijeraPiedraPapel {
+
+
+    public static void main(String[] args) {
+        piedrapapeltijera game = new piedrapapeltijera();
+
+    }
+}
---
+++ src/division.java
@@ -0,0 +1,10 @@
+public class division {
+    public static void main(String[] args) {
+       int num1 =0;
+       int num2  =100;
+       int resultado = 0;
+        resultado =  num1/num2;
+        System.out.println(resultado);
+
+    }
+}
