bd9e6107c7eec6c8b44cc5a4207197f52b43effa
edwinmgallego <ing.edwin.gallego@gmail.com>
2022-08-31 19:31:13 -0500

    ingreso de datos  para  el arreglo

--- src/main.java
+++ src/main.java
@@ -1,21 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-        int[] arr = new int[]{12,3,5,21,4,85,6,9,2,1};
-        for (int i : arr) {
-            System.out.print(i+" ");
+        Scanner entradaDeDatos = new Scanner(System.in);
+        int[] miArreglo = new int[10];
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Ingrese el número en la posición " + (i) + " :");
+            miArreglo[i] = entradaDeDatos.nextInt();
+            System.out.println(miArreglo[i]);
         }
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[i] < arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-            }
+
         }
-        System.out.println("\nAfter Sorting...");
-        for (int i : arr) {
-            System.out.print(i+" ");
-        }
-    }
+
 }
