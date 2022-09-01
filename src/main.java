f0f6db95c598209b4a937dfd82f58465ebc9abfd
edwinmgallego <ing.edwin.gallego@gmail.com>
2022-08-31 19:56:01 -0500

    arreglo de menor a mayor

--- src/main.java
+++ src/main.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
+
+        Scanner opcionesDeOrdenamiento = new Scanner(System.in);
+
         Scanner entradaDeDatos = new Scanner(System.in);
         int[] miArreglo = new int[10];
         for (int i = 0; i < 10; i++) {
@@ -11,6 +14,49 @@ public class main {
             System.out.println(miArreglo[i]);
         }
 
+
+
+        System.out.println("digite  que  forma  quiere  ordenar");
+        System.out.println("1. menor a mayor");
+        System.out.println("2. mayor a menor");
+        System.out.println("3. salir");
+        int opciones = opcionesDeOrdenamiento.nextInt();
+
+
+
+
+        //opciones para   ordenar
+        switch (opciones){
+            case 1 :
+                menorAMayor(miArreglo);
+                break;
+
+            case 2:
+                break;
+
+            case 0:
+                break;
         }
+
+
+        }
+
+       public static void menorAMayor(int[] ArregloVector){
+           for (int i = 0; i < ArregloVector.length; i++) {
+               for (int j = 0; j < ArregloVector.length; j++) {
+                   if (ArregloVector[i] < ArregloVector[j]) {
+                       int temp = ArregloVector[i];
+                       ArregloVector[i] = ArregloVector[j];
+                       ArregloVector[j] = temp;
+                   }
+               }
+           }
+           System.out.println("\ndespues de arreglado...");
+           for (int i : ArregloVector) {
+               System.out.print(i+" ");
+           }
+        }
+
+
 
 }
